0f466806f303fe739db8d61c9c78d170cd63c938
thu <thu.vo25@student.passerellesnumeriques.org>
2024-01-30 20:59:25 +0700

    bai3

--- test/routes/web.php
+++ test/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/unicode',function(){
+     return view('home');
+});
+Route::get('/product',function(){
+    return view('product');
 });
