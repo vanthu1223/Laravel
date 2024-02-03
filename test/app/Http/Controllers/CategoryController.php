28373676525b930c1b38a2ec34e337b4f9afba5f
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-02-03 13:25:33 +0700

    Laravel bai10 (#9)
    * Laravel-bai10
    
    * Lavrravel-bai10

--- test/app/Http/Controllers/CategoryController.php
+++ test/app/Http/Controllers/CategoryController.php
@@ -40,8 +40,14 @@ class CategoryController extends Controller
         // $input= $request->input();
         // dd($input);
 
-        $id=$request->input('id.1.name');
-        dd($id);
+        // $id=$request->input('id.1.name');
+        // dd($id);
+
+        // $id=$request->query('id');
+
+        // $query = $request->query();
+        // dd($query);
+        
         return view('clients/category/list');
     }
     
@@ -58,12 +64,39 @@ class CategoryController extends Controller
     }
     // Thêm dữ liệu vào chuyên mục phương thức post
     public function addCategory(Request $request){
-        $allData = $request->all();
-        dd($allData);
+        // $allData = $request->all();
+        // dd($allData);
+        // $cateName = $request->id;
+        // dd($cateName);
+        if($request->has('category_name')){
+            $request->flash();
+            // dd($cateName);
+          return  redirect(route('category.add'));
+        }
+        else {
+            return 'Không có category name';
+        }
+
+
+        $old = $request->old('category_name');
         return view('clients/category/add');
     }
     // Xóa dữ liệu bằng pt delete
     public function deleteCategory($id){
+    }
+    public function getFile(){
+        return view('clients/category/file');
+    }
+    public function Handlefile(Request $request){
+       // $file = $request->file();
+       
+        // dd($file);
+        if ($request->hasFile('photo')){
+            $file = $request->file();
+        }
+        else{
+            return "chọn file";
+        }
     }
 
 }
