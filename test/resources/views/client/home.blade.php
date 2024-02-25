e1fc7e1c1a36332955841ac8138f7b2c08c16d2e
thu <thu.vo25@student.passerellesnumeriques.org>
2024-02-25 18:06:08 +0700

    Laravel-bai16

--- test/resources/views/client/home.blade.php
+++ test/resources/views/client/home.blade.php
@@ -13,6 +13,13 @@
 <h1> Trang chủ </h1>
 @datatime('2022-12-03 15:00:00')
 @datatime("2022-12-06 15:00:00")
+@env(production')
+<p>Môi trường production </p>
+@elseenv('test')
+<p>Không phải mt test</p>
+@else 
+<p>Không phải dev</p>
+@endenv
 
 @endsection
 
