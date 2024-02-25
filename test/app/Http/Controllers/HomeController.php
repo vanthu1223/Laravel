e301cc7b603d7d30408c8fc044a1238d8b6b9fb9
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-02-25 19:51:46 +0700

    Laravel-18 (#17)

--- test/app/Http/Controllers/HomeController.php
+++ test/app/Http/Controllers/HomeController.php
@@ -7,24 +7,38 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
-    public $data =[];
-    public function index(){
-        $this->data['title']= 'Lập trình tại unicode';
+    public $data = [];
+    public function index()
+    {
+        $this->data['title'] = 'Lập trình tại unicode';
         $this->data['message'] = "Đăng ký tài khoản thành công";
-        return view('client.home',$this->data);
+        return view('client.home', $this->data);
     }
-    public function products(){
+    public function products()
+    {
         $this->data['title'] = 'Sản phẩm';
-        return view('client.products',$this->data);
+        return view('client.products', $this->data);
     }
-    public function getAdd(){
+    public function getAdd()
+    {
         $this->data['title'] = 'Thêm sản phẩm';
-        return view('client.add',$this->data);
+        return view('client.add', $this->data);
     }
-    public function postAdd(Request $request){
+    public function postAdd(Request $request)
+    {
         dd($request);
     }
-    public function putAdd(Request $request){
+    public function putAdd(Request $request)
+    {
         dd($request);
+    }
+    public function getArray()
+    {
+        $contentArray = [
+            'name' => 'Laravel 8.x',
+            'lesson' => 'Khóa học lập trình',
+            'academy' => 'unicode âcdemy'
+        ];
+        return $contentArray;
     }
 }
