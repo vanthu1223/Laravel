20f1e3fa21b173dcb985d8ccc92e4798cf8073a2
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-02-26 14:16:51 +0700

    Laravel-bai22 (#21)

--- test/app/Http/Controllers/HomeController.php
+++ test/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
 
 class HomeController extends Controller
 {
@@ -25,20 +26,28 @@ class HomeController extends Controller
         $this->data['errorMessage'] = 'Vui lòng nhập thông tin  ';
         return view('client.add', $this->data);
     }
-    public function postAdd(Request $request)
+    public function postAdd(ProductRequest $request)
     {
-        $rules = [
-            'product_name' => 'required|min:6',
-            'product_price' => 'required|integer'
-        ];
-        $message = [
-            'product_name.required' => 'Tên sản phẩm bắt buộc phải nhập',
-            'product_name.min' => 'Tên sản phẩm không được nhỏ hơn :min ký tự',
-            'product_price.required' => 'giá sản phẩm bắt buộc phải nhập',
-            'product_name.integer' => 'Giá sản phẩm phải là số'
-        ];
-        $request->validate($rules,$message);
+        dd($request);
+        // $rules = [
+        //     'product_name' => 'required|min:6',
+        //     'product_price' => 'required|integer'
+        // ];
+        // $message = [
+        //     'product_name.required' => 'Tên sản phẩm bắt buộc phải nhập',
+        //     'product_name.min' => 'Tên sản phẩm không được nhỏ hơn :min ký tự',
+        //     'product_price.required' => 'giá sản phẩm bắt buộc phải nhập',
+        //     'product_name.integer' => 'Giá sản phẩm phải là số'
+        // ];
+        // $message = [
+        //     'required' => 'Tên sản phẩm bắt buộc phải nhập',
+        //     'min' => 'Tên sản phẩm không được nhỏ hơn :min ký tự',
+        //     'integer' => 'Giá sản phẩm phải là số'
+        // ];
+        // $request->validate($rules,$message);
         // Xử lý việc thêm dữ liệu vào database
+
+
 
     }
     public function putAdd(Request $request)
