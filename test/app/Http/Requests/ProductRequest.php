c82fcca4b1beabb11a38567e76756af47e29d15b
thu <thu.vo25@student.passerellesnumeriques.org>
2024-02-26 14:15:57 +0700

    Laravel-bai22

---
+++ test/app/Http/Requests/ProductRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'product_name' => 'required|min:6',
+            'product_price' => 'required|integer'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'product_name.required' => 'Trường :attribute bắt buộc phải nhập',
+            'product_name.min' => 'Tên sản phẩm không được nhỏ hơn :min ký tự',
+            'product_price.required' => 'Trường :attribute bắt buộc phải nhập',
+            'product_name.integer' => 'Trường :attribute  phải là số'
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'product_name' => 'Tên sản phẩm',
+            'product_price'=> 'Giá sản phẩm' 
+        ];
+    }
+}
