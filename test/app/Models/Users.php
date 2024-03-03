e875e11253177db14175b9942d2b08588a76ca48
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-03-03 18:07:19 +0700

    Laravel bai34 (#34)
    * Laravel-bai34
    
    * Laravel-bai34

--- test/app/Models/Users.php
+++ test/app/Models/Users.php
@@ -5,45 +5,59 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+
 class Users extends Model
 {
     use HasFactory;
     protected $table = 'users';
-    public function getAllUser(){
+    public function getAllUser()
+    {
         $users = DB::select('SELECT * from users ORDER BY create_at DESC');
         return $users;
     }
-    public function addUser($data){
-        Db::insert('INSERT INTO users (fullname,email,create_at) value (?,?,?)',$data);
+    public function addUser($data)
+    {
+        Db::insert('INSERT INTO users (fullname,email,create_at) value (?,?,?)', $data);
     }
-    public function getDetial($id){
+    public function getDetial($id)
+    {
         return DB::select('SELECT * FROM ' . $this->table . ' WHERE id = ?', [$id]);
     }
-    public function updateUser($data,$id){
-        $data = array_merge($data,[$id]);
-        return DB::update('UPDATE '.$this->table.' SET fullname =?,email =?, update_at= ? where id=?', $data);
+    public function updateUser($data, $id)
+    {
+        $data = array_merge($data, [$id]);
+        return DB::update('UPDATE ' . $this->table . ' SET fullname =?,email =?, update_at= ? where id=?', $data);
     }
-    public function deleteUser($id){
-      return  DB::delete("DELETE FROM $this->table WHERE id=? ",[$id]);
+    public function deleteUser($id)
+    {
+        return  DB::delete("DELETE FROM $this->table WHERE id=? ", [$id]);
     }
-    public function learnQueryBuiler(){
+    public function learnQueryBuiler()
+    {
         // lấy tất cả bản ghi của table
-        $id=20;
-       $lists = DB::table($this->table)
-       ->select('fullname','email')
+        $id = 20;
+        // $lists = DB::table($this->table)
+        //     ->select('fullname', 'email','update_at','create_at')
         // ->where('id',2)
         // ->where(function($query) use ($id){
         //     $query->where('id', '<',$id)->orWhere('id','>',$id);
         //     $query->orWhere('id','>',$id);
         // })
-       // ->where('fullname','like','%Vân Thư%')
-    //    ->whereBetween('id',[1,4])
-    ->whereIn('id',[1,4])
-        ->get();
-       $sql = DB::getQueryLog();
-       
-       // Lấy 1 bản ghi đầu tiên của table lấy thông tin chi tiết
-       $detail = DB::table($this->table)->first();
-
+        // ->where('fullname','like','%Vân Thư%')
+        //    ->whereBetween('id',[1,4])
+        // ->whereIn('id',[1,4])
+        // ->whereDate('update','2023-03-02')
+        // ->whereMonth('create_at','02')
+        // ->whereDay('create_at','18')
+        // ->whereColumn('create_at','>','update_at'
+        //     ->get();
+        //Join 2 bảng lại với nhau
+        $lists =  DB::table('users')
+            ->select('users.*', 'groups.name as group_name')
+            ->rightJoin('groups', 'users.group_id', '=', 'groups.id');
+        $sql = DB::getQueryLog();
+        dd($sql);
+        // Lấy 1 bản ghi đầu tiên của table lấy thông tin chi tiết
+        $detail = DB::table($this->table)->first();
     }
 }
