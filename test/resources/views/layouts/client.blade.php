d40e2beb190a0ae62616e95f6eff3dada74161fb
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-03-11 21:27:25 +0700

    laravel-bai39 (#39)

--- test/resources/views/layouts/client.blade.php
+++ test/resources/views/layouts/client.blade.php
@@ -7,6 +7,7 @@
     <title> Trang web của tou</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('asserts/clients/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('asserts/clients/css/style.css') }}">
     <style type="text/css">
         @yield('css');
     </style>
