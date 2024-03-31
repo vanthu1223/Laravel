381a47266c0c3821790d49c526d4c216da0ec6fe
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-03-31 17:37:28 +0700

    Laravel-45 (#45)

--- test/app/Http/Controllers/PostController.php
+++ test/app/Http/Controllers/PostController.php
@@ -4,16 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index(){
+      echo '<h2> Query Eloquent Model </h2>';
+    //   $allPosts = Post::all();
+    //   if ($allPosts->count()>0){
+    //     foreach($allPosts as $item ){
+    //         echo $item->title.'</br>';
+    //     }
+    //   }
+
+    // $detail = Post::find(1);
+
+    // Sử dụng query builder
+
+    //$activePosts = DB::table('posts')->where('status',1)->get();
+        // $activePosts = Post::where('status')->orderBy('id','DESC')->get();
+        //    if ($activePosts->count()>0){
+        // foreach($activePosts as $item ){
+        //     echo $item->title.'</br>';
+        // }
+      //}
+
         // $allPosts = Post::all();
-        // $post = Post::find(1);
-        $post = new Post;
-        $post->title ='bài viết 3';
-        $post->content ="Nội dung 3";
-        $post->status = 1; 
-        
+        // $activePosts = $allPosts->reject(function ($activePosts){
+        //     return $activePosts->status==1;
+        // });
+        // dd($activePosts);
+
+        // Post::chuck(2,function ($posts){
+        //     foreach ($posts as $post) {
+        //         echo $post->title."</br>";
+        //     }
+        //     echo  'Kết thúc';
+        // });
+
+        $allPosts = Post::where('status',1)->cursor();
+
+        foreach($allPosts as $item) {
+            echo $item->title.'</br>';
+        }
     }
 }
